7af93b2b3af1ea6bb4c1d0ecbcab37eadded6588
jacenfox <jacenfox@gmail.com>
2017-01-04 09:41:33 -0500

    python3 support, more useful output, use -p 2 for mitsuba

--- batchRender.py
+++ batchRender.py
@@ -22,24 +22,24 @@ def parseRenderCmds(PATH_xml, PATH_png, skipExist):
         outfullfile = join(PATH_png, xmlfile)
         outfullfile = outfullfile.replace('.xml', '.' + outputformat)  # does not matter
         if isfile(outfullfile) is True and skipExist is True:
-            print 'skip ' + outfullfile
+            print('skip ' + outfullfile)
             continue
-        renderCmds.append("mitsuba -q " + xmlfullfile + " -o " + outfullfile)
+        renderCmds.append("mitsuba -q -p 2 " + xmlfullfile + " -o " + outfullfile)
     return renderCmds
 
 
 def render(params):
     renderCmd = params
     try:
-        print 'rendering: ' + renderCmd
+        print('rendering: ' + renderCmd)
         os.system(renderCmd)
     except:
-        print 'render failed: ' + renderCmd
+        print('render failed: ' + renderCmd)
 
 
 def printHelp():
-    print 'usage:\n\tpython batchRender.py *inputXMLs_path*.xml *outputImg_path* '\
-          '-j4 [-sjpg]\n\t-j4 pool size 4\n\t-sjpg[png] skip if .jpg[.png] exist'
+    print('usage:\n\tpython batchRender.py *inputXMLs_path*.xml *outputImg_path* '
+          '-j4 [-sjpg]\n\t-j4 pool size 4\n\t-sjpg[png] skip if .jpg[.png] exist')
 
 
 if __name__ == '__main__':
@@ -52,21 +52,21 @@ if __name__ == '__main__':
     # print sys.argv, first one is the name of this script
     if len(sys.argv) >= 4 and '-j' in sys.argv[3]:
         poolNum = int(sys.argv[3][2:])
-        print 'using pool ' + str(poolNum)
+        print('using pool ' + str(poolNum))
 
     skipExist = False
     if len(sys.argv) >= 5 and '-s' in sys.argv[4]:
         outputformat = sys.argv[4][2:]
         skipExist = True
-        print 'will skip the existing .' + outputformat
+        print('will skip the existing .' + outputformat)
 
     renderCmds = parseRenderCmds(PATH_xml, PATH_img, skipExist)
 
     params = renderCmds
     NUM_COUNT_ALL = len(params)
-    print 'wait'
+    print('wait')
     time.sleep(5)
-
+    start_time = time.time()
     _debug = DEBUG
     if _debug:
         render(params[0])
@@ -80,7 +80,10 @@ if __name__ == '__main__':
         if (rs.ready()):
             break
         remaining = rs._number_left
-        print "\nWaiting for", remaining, "tasks to complete in %d\n" % (int(NUM_COUNT_ALL / chunksize + 1))
-        time.sleep(5)
+        time_elapsed = time.time() - start_time
+        time_left = time_elapsed * (remaining / (int(NUM_COUNT_ALL) - remaining))
+        print("Task %d\\%d.\tTime start %.2f\telapsed%.2f\tleft%.2f\n" %
+              (remaining * chunksize, int(NUM_COUNT_ALL + 1), start_time, time_elapsed, time_left))
+        time.sleep(10)
 
-    print str(NUM_COUNT_ALL) + ' scenes done!'
+    print(str(NUM_COUNT_ALL) + ' scenes done!')
